3a345cf3a7b7eaa452195fb2c99e1f51d285b612
amrsaleh__ <a-amsale@microsoft.com>
2020-01-14 18:17:08 +0200

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=vacationhelperbot-Web-Deploy.pubxml -p:Password=e7yoqKkEGhx1tYABrrZ1bGfYTEC5Zkapw9RQKBQrriGit9109JJEt9nC8R6G
+
